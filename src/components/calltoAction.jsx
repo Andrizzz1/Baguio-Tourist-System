c1e3fb38a95f21150ee484b7b2ffa5f93629d462
unknown <delacruztristan02@gmail.com>
2026-04-22 02:47:21 +0800

    Changing the fonts

--- src/components/calltoAction.jsx
+++ src/components/calltoAction.jsx
@@ -3,7 +3,7 @@ export const CalltoAction = ()=>{
         <div className="bg-green-900 text-white p-10 rounded-3xl">
             <div className="max-w-4xl mx-auto text-center">
                 <h2 className="font-bold text-3xl">Ready to Explore Baguio?</h2>
-                <p className="mt-2 text-green-200">Join thousands of travelers who have discovered the best of Baguio with our AI-powered guide.</p>
+                <p className="mt-2 text-black-200">Join thousands of travelers who have discovered the best of Baguio with our AI-powered guide.</p>
                 <button className="mt-5 bg-white text-green-900 py-2 px-6 rounded-4xl hover:bg-green-200 transition-colors">Start Your Journey</button>
             </div>
         </div>
