f824719051a361c1c09d5afc735893fa500108f2
John Andrei Mandapat <andreimandapat09@gmail.com>
2026-05-02 22:32:55 +0800

    Dashboard redesign (#47)
    * feat:Call to action and footer
    
    * update for vercel.json
    
    * fix:small adjustments
    
    * fix:bugs
    
    * feat:Dashboard for user
    
    * fix vercel routing
    
    * fix vercel routing
    
    * fix:register.js bug
    
    * fix:register.js bug
    
    * fix:register.js bug
    
    * dashboard redesign
    
    * feat:added featureTourist spots

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,10 +1,12 @@
 import { DashboardNav } from "./DashboardNav.jsx";
 import { NameCard } from "./namecard.jsx";
 import { QuickAction } from "./QuickActions.jsx";
+import { FeaturedDashboardSpots } from "./FeaturedDashboardSpots.jsx";
 export const Dashboard = ()=>{
-    return <section className="bg-white min-h-screen ">
+    return <section className="bg-gray-50 min-h-screen pb-10">
         <DashboardNav/>
         <NameCard />
         <QuickAction  />
+        <FeaturedDashboardSpots />
     </section>
 }
--- src/dashboard/namecard.jsx
+++ src/dashboard/namecard.jsx
@@ -2,11 +2,11 @@ export const NameCard = ()=>{
     const user = JSON.parse(localStorage.getItem('user'))
     const firstName = user?.username?.split(' ')[0] || 'User'
     return <section className="text-black flex justify-center m-5">
-        <div className="relative overflow-hidden p-10 rounded-3xl bg-cover bg-center text-white w-7xl" 
+        <div className="relative overflow-hidden p-10 rounded-3xl h-72 grid bg-cover bg-center text-white w-7xl" 
         style={{backgroundImage:`linear-gradient(120deg, rgba(4, 120, 87, 0.92), rgba(6, 95, 70, 0.72)),url('/imgs/bg.png')`}}>
-            <div className="text-white text-sm bg-emerald-500 w-fit py-1 px-2  rounded-2xl">weather</div>
+            <div className="text-white text-sm bg-emerald-500 w-fit h-fit py-1 px-2  rounded-2xl">weather</div>
                 <div>
-                    <h1 className="font-semibold text-emerald-50 text-2xl">Kamusta, {firstName}!</h1>
+                    <h1 className="font-semibold text-emerald-50 text-5xl">Kamusta, {firstName}!</h1>
                 </div>
             <p className="max-w-xl text-emerald-100">Ready to explore the Summer Capital? Discover hidden trails, local cuisine, and the rich Cordilleran history — all guided by AI.</p>
         </div>
