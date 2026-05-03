0eae83904b15b3a951951bc5ccde938848f0e24b
Andrizzz1 <andreimandapat09@gmail.com>
2026-05-03 17:32:33 +0800

    feat:dashboard chatbot and Weather Api inserted

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,9 @@ import { RegisterAcc } from './CreateAccount.jsx'
 import { Signin } from './Signin.jsx'
 import { Dashboard } from './dashboard/dashboard.jsx'
 import { ProtectedRoute } from './auth/ProtectedRoute.jsx'
+import { Community } from './navpages/community.jsx'
+import { Saved } from './navpages/Saved.jsx'
+import { Explore } from './navpages/Explore.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
@@ -21,7 +24,23 @@ createRoot(document.getElementById('root')).render(
             <Dashboard />
           </ProtectedRoute>
         }/>
+        <Route path='/Community' element={ 
+          <ProtectedRoute>
+            <Community />
+          </ProtectedRoute>
+        }/>
+        <Route path='/explore' element={ 
+          <ProtectedRoute>
+            <Explore />
+          </ProtectedRoute>
+        }/>
+        <Route path='/saved' element={ 
+          <ProtectedRoute>
+            <Saved  />
+          </ProtectedRoute>
+        }/>
       </Routes>
+     
     </BrowserRouter>
    
   </StrictMode>,
