89b4c01289f385c0e2798840d32b7d220efd2b72
Andrizzz1 <andreimandapat09@gmail.com>
2026-05-17 01:50:51 +0800

    fix:login.js

--- api/login.js
+++ api/login.js
@@ -1,5 +1,6 @@
 import pkg from 'pg'
 import bcrypt from 'bcrypt'
+
 const { Pool } = pkg
 
 const pool = new Pool({
@@ -8,11 +9,17 @@ const pool = new Pool({
 })
 
 export default async function handler(req, res) {
-    if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' })
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Method not allowed' })
+    }
 
     const { email, password } = req.body
+
     try {
-        const result = await pool.query('SELECT * FROM users WHERE email = $1', [email])
+        const result = await pool.query(
+            'SELECT * FROM users WHERE email = $1',
+            [email]
+        )
 
         if (result.rows.length === 0) {
             return res.status(400).json({ error: 'Email not found' })
@@ -25,9 +32,16 @@ export default async function handler(req, res) {
             return res.status(400).json({ error: 'Incorrect password' })
         }
 
-        res.json({ message: 'Login successful', user: { id: user.id, email: user.email,username: user.username } })
+        return res.json({
+            message: 'Login successful',
+            user: {
+                id: user.users_id,
+                email: user.email,
+                username: user.username
+            }
+        })
 
     } catch (error) {
-        res.status(500).json({ error: 'Login failed' })
+        return res.status(500).json({ error: 'Login failed' })
     }
 }
